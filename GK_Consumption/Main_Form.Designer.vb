79528bd9621fef5c6aa2f163a59e6349a410562a
anton <anton@LT-AV>
2024-09-17 08:55:10 +0200

    Add project files.

---
+++ GK_Consumption/Main_Form.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MonthCalendar = New MonthCalendar()
+        Selection_Text = New Label()
+        Selection_From = New Label()
+        Selection_To = New Label()
+        Txt_Read = New Label()
+        GB_Usage = New GroupBox()
+        SuspendLayout()
+        ' 
+        ' MonthCalendar
+        ' 
+        MonthCalendar.Location = New Point(18, 66)
+        MonthCalendar.MaxSelectionCount = 366
+        MonthCalendar.Name = "MonthCalendar"
+        MonthCalendar.TabIndex = 0
+        ' 
+        ' Selection_Text
+        ' 
+        Selection_Text.AutoSize = True
+        Selection_Text.Location = New Point(18, 9)
+        Selection_Text.Name = "Selection_Text"
+        Selection_Text.Size = New Size(204, 15)
+        Selection_Text.TabIndex = 1
+        Selection_Text.Text = "Please select period with the calender"
+        ' 
+        ' Selection_From
+        ' 
+        Selection_From.AutoSize = True
+        Selection_From.Location = New Point(18, 24)
+        Selection_From.Name = "Selection_From"
+        Selection_From.Size = New Size(12, 15)
+        Selection_From.TabIndex = 2
+        Selection_From.Text = "-"
+        ' 
+        ' Selection_To
+        ' 
+        Selection_To.AutoSize = True
+        Selection_To.Location = New Point(18, 39)
+        Selection_To.Name = "Selection_To"
+        Selection_To.Size = New Size(12, 15)
+        Selection_To.TabIndex = 3
+        Selection_To.Text = "-"
+        ' 
+        ' Txt_Read
+        ' 
+        Txt_Read.AutoSize = True
+        Txt_Read.Location = New Point(18, 434)
+        Txt_Read.Name = "Txt_Read"
+        Txt_Read.Size = New Size(12, 15)
+        Txt_Read.TabIndex = 4
+        Txt_Read.Text = "_"
+        ' 
+        ' GB_Usage
+        ' 
+        GB_Usage.Location = New Point(267, 9)
+        GB_Usage.Name = "GB_Usage"
+        GB_Usage.Size = New Size(585, 440)
+        GB_Usage.TabIndex = 5
+        GB_Usage.TabStop = False
+        GB_Usage.Text = "Consumption"
+        ' 
+        ' Main_Form
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(864, 461)
+        Controls.Add(GB_Usage)
+        Controls.Add(Txt_Read)
+        Controls.Add(Selection_To)
+        Controls.Add(Selection_From)
+        Controls.Add(Selection_Text)
+        Controls.Add(MonthCalendar)
+        Name = "Main_Form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MonthCalendar As MonthCalendar
+    Private WithEvents Selection_Text As Label
+    Private WithEvents Selection_From As Label
+    Private WithEvents Selection_To As Label
+    Private WithEvents Txt_Read As Label
+    Friend WithEvents GB_Usage As GroupBox
+
+End Class
---
+++ GK_Consumption/Main_Form.vb
@@ -0,0 +1,169 @@
+﻿Imports System.Globalization
+Imports System.IO
+Imports System.Net.Security
+
+Public Class Main_Form
+
+    Const NumIngredients As Integer = 30
+
+    Public InstalledIngredients(NumIngredients) As Boolean
+    Public Period_Consumption(NumIngredients) As Double
+    Public LogDir As String = "C:\GK\GK295\LOG\"
+    Public TxtFileName As String = "C:\GK\GK.TXT"
+    Public IngrNames(NumIngredients) As String
+    Public Offset As Integer = 0
+
+
+    Private Sub MonthCalendar_DateChanged(sender As Object, e As DateRangeEventArgs) Handles MonthCalendar.DateChanged
+        Selection_From.Text = MonthCalendar.SelectionRange.Start
+        Selection_To.Text = MonthCalendar.SelectionRange.End
+        Selection_Text.Text = "Selected period:"
+        GetConsumption()
+    End Sub
+
+    Private Async Sub GetConsumption()
+        Dim LogFileName As String
+        Dim Selectiondate As Date
+        Dim pos, pos2 As Integer
+        Dim ErrorInLine As Boolean
+        Dim line As String
+        Dim DecPoint As String = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator
+        Dim IngrNr As Integer
+        Dim Value As Double
+        For IngrNr = 0 To NumIngredients
+            Period_Consumption(IngrNr) = 0
+            InstalledIngredients(IngrNr) = False
+        Next
+
+        Selectiondate = MonthCalendar.SelectionRange.Start
+        While DateDiff("d", Selectiondate, MonthCalendar.SelectionRange.End) >= 0
+            LogFileName = IO.Path.Combine(LogDir, Selectiondate.ToString("yyyyMMdd") & ".LOG")
+            If IO.File.Exists(LogFileName) Then
+                Txt_Read.Text = "Reading " & LogFileName
+                Await Task.Delay(100)
+                Using Reader As StreamReader = New StreamReader(LogFileName)
+                    While Not Reader.EndOfStream
+
+                        line = Reader.ReadLine
+                        'First remove timestamp & MessID
+                        pos = InStr(line, "#") ' search for MessID
+                        If pos > 0 Then
+                            pos2 = InStr(pos, line, ";") ' search for MessID end
+                            If (pos2 - pos) <> 4 Then
+                                'Invalid messageID
+                                ErrorInLine = True
+                            Else
+                                'Remove time & MessID
+                                line = Mid(line, pos2 + 1)
+                            End If
+                        Else
+                            'No messageID, Remove time 
+                            line = Mid(line, 7)
+                        End If
+
+                        Dim LineArray() As String = Split(line, ";")
+                        If LineArray(0) = "I" Or LineArray(0) = "P" Or LineArray(0) = "G" Then
+                            IngrNr = LineArray(2)
+                            If Not Double.TryParse(LineArray(4).Replace(".", DecPoint), Value) Then
+                                Txt_Read.Text = "Could not parse " & LineArray(4).Replace(".", DecPoint)
+                                Await Task.Delay(100)
+                            Else
+                                'add the value to the array
+                                Period_Consumption(IngrNr) = Period_Consumption(IngrNr) + Value
+                                InstalledIngredients(IngrNr) = True
+                                'Txt_Read.Text = Value.ToString
+                                If IngrNr = 2 Then
+                                    Txt_Read.Text = Period_Consumption(IngrNr).ToString
+                                End If
+                            End If
+                        End If
+                    End While
+
+                End Using
+
+            End If
+
+            Selectiondate = Selectiondate.AddDays(1)
+        End While
+        Txt_Read.Text = Period_Consumption(2).ToString
+        CreateConsumptionLines()
+
+    End Sub
+
+    Private Sub CreateConsumptionLines()
+        Dim ypos As Integer = 50
+        Dim n As Integer = 0
+        Dim Lbl As Label
+
+        'DisposeUsage()
+        GB_Usage.Controls.Clear()
+
+        'Offset += 10
+        For i As Integer = 1 To NumIngredients
+            If InstalledIngredients(i) Then
+                n += 1
+                ypos = (n - 1) * 20 + 50 '+ Offset
+                'Add Name
+                Lbl = New Label
+                GB_Usage.Controls.Add(Lbl)
+                With Lbl
+                    .Size = New Size(100, 15)
+                    .Location = New Point(50, ypos)
+                    .Name = "Lbl" & i.ToString
+                    .Text = IngrNames(i)
+                End With
+
+                'Add Value
+                Lbl = New Label
+                GB_Usage.Controls.Add(Lbl)
+                With Lbl
+                    .AutoSize = False
+                    .Size = New Size(100, 15)
+                    .Location = New Point(150, ypos)
+                    .Name = "Lbl" & i.ToString
+                    .Text = Period_Consumption(i).ToString("F2")
+                    .TextAlign = ContentAlignment.MiddleRight
+                End With
+
+            End If
+        Next
+    End Sub
+
+
+    Private Sub ReadIngrNames()
+        Dim Line As String
+        Dim n As Integer
+
+        For i As Integer = 0 To NumIngredients
+            IngrNames(i) = ""
+        Next
+        Using Reader As StreamReader = New StreamReader(TxtFileName)
+            While Not Reader.EndOfStream
+                Line = Reader.ReadLine
+                Dim LineArray() As String = Split(Line, "=")
+                If Len(Trim(LineArray(0))) > 11 Then
+                    If Mid(Trim(LineArray(0)), 1, 11) = "Ingredient_" Then
+                        If Integer.TryParse(Mid(Trim(LineArray(0)), 12), n) Then
+                            IngrNames(n) = LineArray(1)
+                        End If
+                    End If
+                End If
+                'IngrNames(NumIngredients) = ""
+            End While
+        End Using
+
+    End Sub
+
+    Private Async Sub DisposeUsage()
+        'For Each item In Me.GB_Usage.Controls '.OfType(Of Label)()
+        '    item.Dispose()
+        'Next
+    End Sub
+
+
+
+    Private Sub Main_Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisposeUsage()
+        ReadIngrNames()
+    End Sub
+End Class
